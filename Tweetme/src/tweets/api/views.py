7726e4a08b02d7c260734aa633993c99adfe10f5
Bojan Jovanovic <bojanjovanovic@Bojans-MacBook-Pro.local>
2017-01-16 20:59:29 -0600

    User API tweet, Like Tweet, Display likes & Verb

--- Tweetme/src/tweets/api/views.py
+++ Tweetme/src/tweets/api/views.py
@@ -11,6 +11,16 @@ from .pagination import StandardResultsPagination
 from .serializers import TweetModelSerializer
 
 
+class LikeToggleAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, pk, format=None):
+        tweet_qs = Tweet.objects.filter(pk=pk)
+        message = "Not allowed"
+        if request.user.is_authenticated():
+            is_liked = Tweet.objects.like_toggle(request.user, tweet_qs.first())
+            return Response({'liked': is_liked})
+        return Response({"message": message}, status=400)
+
 class RetweetAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request, pk, format=None):
@@ -18,7 +28,7 @@ class RetweetAPIView(APIView):
         message = "Not allowed"
         if tweet_qs.exists() and tweet_qs.count() == 1:
             #if request.user.is_authenticated():
-            new_tweet = Tweet.objects.retweet(request.user, tweet_qs.first())
+            new_tweet = Tweet.objects.like_toggle(request.user, tweet_qs.first())
             if new_tweet is not None:
                 data = TweetModelSerializer(new_tweet).data
                 return Response(data)
@@ -38,11 +48,21 @@ class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     pagination_class = StandardResultsPagination
 
+    def get_serializer_context(self, *args, **kwargs):
+        context = super(TweetListAPIView, self).get_serializer_context(*args, **kwargs)
+        context['request'] = self.request
+        return context
+
     def get_queryset(self, *args, **kwargs):
-        im_following = self.request.user.profile.get_following() # none
-        qs1 = Tweet.objects.filter(user__in=im_following)
-        qs2 = Tweet.objects.filter(user=self.request.user)
-        qs = (qs1 | qs2).distinct().order_by("-timestamp")
+        requested_user = self.kwargs.get("username")
+        if requested_user:
+            qs = Tweet.objects.filter(user__username=requested_user).order_by("-timestamp")           
+        else:    
+            im_following = self.request.user.profile.get_following() # none
+            qs1 = Tweet.objects.filter(user__in=im_following)
+            qs2 = Tweet.objects.filter(user=self.request.user)
+            qs = (qs1 | qs2).distinct().order_by("-timestamp")
+
         query = self.request.GET.get("q", None)
         if query is not None:
             qs = qs.filter(
